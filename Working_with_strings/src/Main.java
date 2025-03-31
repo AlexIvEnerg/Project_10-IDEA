d86293709508034a61950f16f5501f5800890ad1
Алексей Елютин <elyutin91@mail.ru>
2025-03-31 16:55:14 +0300

    Changing a new folder in Project_10

--- Working_with_strings/src/Main.java
+++ Working_with_strings/src/Main.java
@@ -15,5 +15,17 @@ public class Main {
         System.out.println(str);
         str = str.toLowerCase();  //  H->h
         System.out.println(str);
+
+        int n1 = 3; int n2 = 56;
+        StringBuilder sb = new StringBuilder(10);
+        String s1 = sb.append(n1).append('+').append(n2).append("=59").toString();
+        System.out.println(s1);  sb = new StringBuilder(10);
+        String s2 = sb.append(n1).append('-').append(n2).append("=-53").toString();
+        System.out.println(s2);  sb = new StringBuilder(10);
+        String s3 = sb.append(n1).append('*').append(n2).append("=168").toString();
+        System.out.println(s3); s1 = s1.replace("=","равно");
+        System.out.println(s1); s2 = s2.replace("=","равно");
+        System.out.println(s2); s3 = s3.replace("=","равно");
+        System.out.println(s3);
     }
 }
