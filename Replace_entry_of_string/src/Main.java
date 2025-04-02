f079b12bd5a5dfa7bdbc2bf25e97fa7f4f9b89ce
Алексей Елютин <elyutin91@mail.ru>
2025-04-02 13:11:53 +0300

    Changing folders to repos. Project_10

--- Replace_entry_of_string/src/Main.java
+++ Replace_entry_of_string/src/Main.java
@@ -1,4 +1,5 @@
 import java.lang.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +8,22 @@ public class Main {
 
         StringBuilder stringBuilder = new StringBuilder(str);
         stringBuilder.replace(str.indexOf("Object-oriented programming",50),162,"OOP");
-
         System.out.println(stringBuilder);
+
+        String str1, str2;   Scanner scan = new Scanner(System.in);
+        System.out.print("Введите строки разной длины: ");
+        str1 = scan.nextLine(); str2 = scan.nextLine();
+        System.out.println(str1 + " " + str2);
+        returnOfChar(str1,str2);
+    }
+
+    public static void returnOfChar(String... args) {
+        for (String str : args) {
+            char ch1 = str.charAt(str.length()/2-1);
+            char ch2 = str.charAt(str.length()/2);
+            char[] arr = {ch1,ch2};
+            String repl = new String(arr);
+            System.out.println("Средние знаки: "+ repl);
+        }
     }
 }
