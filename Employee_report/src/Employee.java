ffbc062b349ea2979918cab752441eb36768f9bf
Алексей Елютин <elyutin91@mail.ru>
2026-01-22 14:55:28 +0300

    Updating a folder the Employee_report

--- Employee_report/src/Employee.java
+++ Employee_report/src/Employee.java
@@ -1,6 +1,20 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
 
 public class Employee {
      String full_name; double salary;
      LocalDate salaryDate = LocalDate.of(2025, 8, 15);
+
+     public void printToFile(String f_name, double s) {
+          try (FileOutputStream os = new FileOutputStream("src\\file.txt", true);
+               PrintWriter pw = new PrintWriter(os)){
+               pw.print(f_name + " , ");
+               pw.println(s);
+          } catch (IOException e) {
+               System.out.print("Exception");
+          }
+     }
+
 }
--- Employee_report/src/Main.java
+++ Employee_report/src/Main.java
@@ -1,3 +1,6 @@
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         Employee[] arr = new Employee[4];
@@ -7,5 +10,18 @@ public class Main {
         arr[2].full_name = "Кларк"; arr[2].salary = 470;
         arr[3].full_name = "Уотсон"; arr[3].salary = 450;
         Report.generateReport(arr); Full_report.generateReport(arr);
+        for (Employee pers : arr) {
+            pers.printToFile(pers.full_name, pers.salary);
+        }
+
+        try (FileReader file = new FileReader("src\\file.txt")){
+            int c;
+            while ((c = file.read())!= -1 ) {
+                System.out.print((char)c);
+            }
+        } catch (IOException e) {
+            System.out.print(e.getMessage());
+        }
+
     }
 }
